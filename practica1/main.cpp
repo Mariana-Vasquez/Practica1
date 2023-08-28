58381f1e090625633cf5dba255ed774d836a8836
Mariana Vasquez <mariana.vasquezc@udea.edu.co>
2023-08-27 19:48:51 -0500

    creacion del caso 2, modificacion en el menu principal

--- practica1/main.cpp
+++ practica1/main.cpp
@@ -9,44 +9,26 @@ int main()
 
     //Declaracion de funciones
     void espera();
+    void menu();
 
     while(opcion!=0){
 
-        //Menu principal
-
-        cout<<"Bienvenid@ al laboratorio 1, por favor ingrese el numero del ejercio que quiere ejecutar: "<<endl;
-        cout<<"1. Identificacion de caracter."<<endl;
-        cout<<"2. Minima conbinacion de billltes y monedas."<<endl;
-        cout<<"3. Conbinacion de mes y dia validos."<<endl;
-        cout<<"4. Reloj digital, cuanto tiempo a pasado."<<endl;
-        cout<<"5. Patron."<<endl;
-        cout<<"6. Serie de Euler."<<endl;
-        cout<<"7. Serie de Fibonacci."<<endl;
-        cout<<"8. Suma de multiplos menores a un numero ingresado."<<endl;
-        cout<<"9. Suma de los cuadrados de los digitos de un numero."<<endl;
-        cout<<"10. El n esimo numero primo."<<endl;
-        cout<<"11. Minimo comun multiplo."<<endl;
-        cout<<"12. Maximo factor primo."<<endl;
-        cout<<"13. Suma de los numeros menores a un numero ingresado."<<endl;
-        cout<<"14. Numero palindromo."<<endl;
-        cout<<"15. Espiral numerico."<<endl;
-        cout<<"16. Serie de Collatz."<<endl;
-        cout<<"17. Secuencia de numeros triangulares."<<endl;
-        cout<<"0. Salir del programa."<<endl;
+        menu();         //menu principal y eleccion de opcion
         cin>>opcion;
 
         system("cls");  //limpia la pantalla
 
+
         //Estructura del menu
 
         switch (opcion) {
-        case 0:
+        case 0:    //Salida del menu
             cout<<"Muchas gracias por haber utilizado hoy nuestros servicios, vuelva pronto. ";
             espera();
             break;
 
         case 1:
-
+        {
             char car;
 
             cout<<"ingrese el caracter a identificar: "<<endl;
@@ -66,9 +48,46 @@ int main()
                 cout<<"El caracter ingresado no es una letra. "<<endl;
 
             espera();
-            break;
+            break;}
 
-        default:
+        case 2:{
+
+            int dinero=0;
+            int contador=0;
+            int denominacion[]={50000,20000,10000,5000,2000,1000,500,200,100,50};
+
+
+            cout<<"Ingrese la cantidad de dinero: "<<endl;
+            cout<<"Solo ingrese los numeros, sin ningun punto, coma o espacio "<<endl;
+            cin>>dinero;
+
+            system("cls");
+
+            cout<<"la denominacion y la cantidad minima de billetes es: "<<endl;
+
+            while(dinero >= 50){
+                for(int i=0; i<10; i++)
+                {
+                    contador = 0;
+                    while(dinero>=denominacion[i]){
+                        contador +=1;
+                        dinero-=denominacion[i];}
+
+                    //salida
+                    cout<<denominacion[i] << " = " << contador<< endl;
+                }
+            }
+
+            if(dinero != 0)
+            cout << "Faltante = " << dinero << endl;
+
+            else
+            cout <<"Faltante = 0"<<endl;
+
+            espera();
+            break;}
+
+        default:  //por defecto cuando se ingresa una opcion invalida
             cout<<"la opcion ingresada no es correcta, intentelo denuevo. ";
             espera();
             break;
@@ -76,6 +95,30 @@ int main()
 
     }
     return 0;
+}
+
+//Declaracion de la funcion
+
+void menu(){
+    cout<<"Bienvenid@ al laboratorio 1, por favor ingrese el numero del ejercio que quiere ejecutar: "<<endl;
+    cout<<"1. Identificacion de caracter."<<endl;
+    cout<<"2. Minima conbinacion de billltes y monedas."<<endl;
+    cout<<"3. Conbinacion de mes y dia validos."<<endl;
+    cout<<"4. Reloj digital, cuanto tiempo a pasado."<<endl;
+    cout<<"5. Patron."<<endl;
+    cout<<"6. Serie de Euler."<<endl;
+    cout<<"7. Serie de Fibonacci."<<endl;
+    cout<<"8. Suma de multiplos menores a un numero ingresado."<<endl;
+    cout<<"9. Suma de los cuadrados de los digitos de un numero."<<endl;
+    cout<<"10. El n esimo numero primo."<<endl;
+    cout<<"11. Minimo comun multiplo."<<endl;
+    cout<<"12. Maximo factor primo."<<endl;
+    cout<<"13. Suma de los numeros menores a un numero ingresado."<<endl;
+    cout<<"14. Numero palindromo."<<endl;
+    cout<<"15. Espiral numerico."<<endl;
+    cout<<"16. Serie de Collatz."<<endl;
+    cout<<"17. Secuencia de numeros triangulares."<<endl;
+    cout<<"0. Salir del programa."<<endl;
 }
 
 void espera(){
